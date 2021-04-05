3a9c309139c6aa8fad2d7f0906123819f7330067
Saksham-Singh123 <76731192+Saksham-Singh123@users.noreply.github.com>
2021-04-05 20:13:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var launcher;
+var dustbinObj, paperObject,groundObject;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(220,350,40);
+	groundObject=new ground(width/2,670,width,20);
+	launcher = new Launcher(paperObject.body,{x:220,y:350});
+
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	Render.run(render);
+  
+} 	
+
+
+function draw() {
+  rectMode(CENTER);
+  background("blue");
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+
+}
+function mouseDragged(){
+	Matter.body.setPosition(paperObject.body,{x:mouseX});
+}
+function mouseReleased(){
+	launcher.fly();
+}
